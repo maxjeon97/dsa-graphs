ea526f0f7f1d37a59e92ca8f03a8ed57cff15071
Cooper Spinelli <cooperspinelli@gmail.com>
2024-04-25 12:25:29 -0600

    All tests passing

--- graphs/fewest-edges/fewestEdges.ts
+++ graphs/fewest-edges/fewestEdges.ts
@@ -1,9 +1,26 @@
 import { UGraphNodeStr } from "../common/graph";
+import { Queue } from "../common/queue";
 
 /** Number of fewest edges between start and end.
  * If no path can be found, return Infinity. */
 
 function fewestEdges(start: UGraphNodeStr, sought: UGraphNodeStr): number {
+  const toSearch = new Queue<[UGraphNodeStr, number]>([[start, 0]]);
+  const seenSet = new Set<UGraphNodeStr>([start]);
+
+  while (!toSearch.isEmpty()) {
+    const [currNode, currDistance] = toSearch.dequeue();
+
+    if (currNode === sought) return currDistance;
+
+    for (const node of currNode.adjacent) {
+      if (!seenSet.has(node)) {
+        toSearch.enqueue([node, currDistance + 1]);
+        seenSet.add(node);
+      }
+    }
+  }
+
   return Infinity;
 }
 
--- graphs/search/search.ts
+++ graphs/search/search.ts
@@ -1,24 +1,70 @@
+import { Stack } from "../common/stack";
 import { UGraphNodeStr } from "../graph/graph";
+import { Queue } from "../common/queue";
 
 /** Return array of nodes, in DFS order (recursive version)  */
 
 function rDfs(
-    start: UGraphNodeStr,
-    result: string[] = [],
-    visited = new Set([start])): string[] {
-  return ["todo"];
+  start: UGraphNodeStr,
+  result: string[] = [],
+  visited = new Set([start])): string[] {
+
+  result.push(start.value);
+
+  for (const node of start.adjacent) {
+    if (!visited.has(node)) {
+      visited.add(node);
+      result = rDfs(node, result, visited);
+    }
+  }
+
+  return result;
 }
 
 /** Return array of nodes, in DFS order (iterative version)  */
 
 function iDfs(start: UGraphNodeStr): string[] {
-  return ["todo"];
+  const toSearch = new Stack<UGraphNodeStr>([start]);
+  const seenSet = new Set<UGraphNodeStr>([start]);
+  const seen = [];
+
+  while (!toSearch.isEmpty()) {
+    const currNode = toSearch.pop();
+
+    seen.push(currNode.value);
+
+    for (const node of currNode.adjacent) {
+      if (!seenSet.has(node)) {
+        toSearch.push(node);
+        seenSet.add(node);
+      }
+    }
+  }
+
+  return seen;
 }
 
 /** Return array of nodes, in BFS order (iterative version)  */
 
 function bfs(start: UGraphNodeStr): string[] {
-  return ["todo"];
+  const toSearch = new Queue<UGraphNodeStr>([start]);
+  const seenSet = new Set<UGraphNodeStr>([start]);
+  const seen = [];
+
+  while (!toSearch.isEmpty()) {
+    const currNode = toSearch.dequeue();
+
+    seen.push(currNode.value);
+
+    for (const node of currNode.adjacent) {
+      if (!seenSet.has(node)) {
+        toSearch.enqueue(node);
+        seenSet.add(node);
+      }
+    }
+  }
+
+  return seen;
 }
 
 
