4ede2468173baba09e9206ab2880dcb1d6e1e434
Max Jeon <maxjeon97@gmail.com>
2024-04-25 11:05:04 -0700

    finish graph

--- graphs/graph/graph.ts
+++ graphs/graph/graph.ts
@@ -21,22 +21,42 @@ class UGraphStr {
 
   /** Add node to graph. */
   addNode(node: UGraphNodeStr): void {
+    this.nodes.add(node);
   }
 
   /** Add array of nodes to graph. */
   addNodes(nodeArray: UGraphNodeStr[]): void {
+    for (const node of nodeArray) {
+      this.addNode(node);
+    }
   }
 
   /** Add edge between v1 and v2. */
   addEdge(v1: UGraphNodeStr, v2: UGraphNodeStr): void {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
+
+  /** Add edges between nodes. */
+  addEdges(edges: UGraphNodeStr[][]): void {
+    for(const edge of edges) {
+      this.addEdge(edge[0], edge[1]);
+    }
   }
 
   /** Remove edge between v1 and v2. */
   removeEdge(v1: UGraphNodeStr, v2: UGraphNodeStr): void {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
   }
 
   /** Remove node from graph. */
   removeNode(node: UGraphNodeStr): void {
+    for(const adjacentNode of node.adjacent) {
+      adjacentNode.adjacent.delete(node);
+      node.adjacent.delete(adjacentNode);
+    }
+    this.nodes.delete(node);
   }
 }
 
